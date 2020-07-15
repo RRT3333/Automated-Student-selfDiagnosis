bf20c83997d30ab4a97712fdc3612850ff2c1c88
TAO.K <48641634+RRT3333@users.noreply.github.com>
2020-07-15 22:32:16 +0900

    Update automated.py

--- automated.py
+++ automated.py
@@ -10,7 +10,6 @@ from selenium import webdriver
 
 driver = webdriver.Chrome("chromedriver.exe")
 def jindan(n,b):
-    # C:\Python\Python38/
     url = "https://eduro.goe.go.kr/stv_cvd_co00_002.do"
     driver.get(url)
     driver.find_element_by_name("frnoRidno").send_keys(b)  # 생년월일
@@ -18,11 +17,8 @@ def jindan(n,b):
     driver.find_element_by_name("schulNm").send_keys('돌마고등학교')  # 학교
     driver.implicitly_wait(3)
     # 확인버튼 xpath 값 가져오기
-    # driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
     driver.find_element_by_xpath('/html/body/app-root/div[2]/section/div/div/div/div[3]/div/button').click()
     driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
-
-    # driver.find_element_by_xpath('/html/body/app-root/div[2]/section/div/div/div/div[3]/div/button').click()
 
     driver.find_element_by_xpath('//*[@id="rspns011"]').click()  # 발열
     driver.find_element_by_xpath('//*[@id="rspns02"]').click()  # 아니오
