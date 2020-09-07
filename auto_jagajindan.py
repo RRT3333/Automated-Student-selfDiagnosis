b64a8095c956772327f08911c8ee21e97563f5cd
TAO.K <48641634+RRT3333@users.noreply.github.com>
2020-09-07 17:14:55 +0900

    Add files via upload

--- auto_jagajindan.py
+++ auto_jagajindan.py
@@ -1,5 +1,5 @@
 '''
-Automated Student self-Diagnosis, ASD
+Automated Student self-Diagnosis, ASD2.0
 Copyright (c)2020 TAO
 All rights reserved.
 '''
@@ -8,19 +8,28 @@ All rights reserved.
 import automated
 f=open("automated_jagajindan_stu_data.txt",'r',encoding='utf-8')
 lines = f.readlines()
-
 i=0
 for line in lines:
-    if(i==0 or i==1):
+    if(i==0 or i==1 or i==2):
         i+=1
         continue
-    if(i%2==0):
-        name=line            #이름
+    #print(line,end='')   #txt파일의 \n도 같이 읽힘에 주의
+    if(i%3==0):
+        #이름
+        name=line
         print(name,end='')
-    else:
-        bit=line            #생년월일
+    elif(i%3==1):
+        #생년월일
+        bit=line
         print(bit,end='')
-        automated.jindan(name, bit)
+    else:
+        #비밀번호
+        pa=line
+        #int(pa)
+        print(line,end='')
+        automated.jindan(name, bit, pa)
     i+=1
 
+print("\n\ndata파일 안에 모든 학생 자가진단을 완료하였습니다.")
+print("즐거운 하루 되십시오.")
 f.close()
