690ba0b4bde86175ee9124de9371288ed9b79cdb
TAO.K <48641634+RRT3333@users.noreply.github.com>
2020-09-07 17:18:29 +0900

    Update automated.py

--- automated.py
+++ automated.py
@@ -1,65 +1,34 @@
+
 '''
-automated student self-diagnosis, ASD2.0
+automated student self-diagnosis, ASD
 Copyright (c)2020 TAO
 All rights reserved.
 '''
 # -*- coding: utf-8 -*-
 # scn_Nm=scn_Nm.decode('a').encode('utf-8')
-#from time import sleep
-#from selenium import webdriver
 from time import sleep
 from selenium import webdriver
-driver = webdriver.Chrome("chromedriver85.exe")	#driver의 version식별을 위해서 사용할 driver의 이름 변경
-def jindan(n,b,p):
-    # C:\Python\Python38/
-    url = "https://hcs.eduro.go.kr/"
-    driver.get(url)
-    sleep(1)
-    driver.find_element_by_xpath('/html/body/app-root/div/div[1]/div[2]/div[1]/section[2]/button[1]').click()
-    driver.find_element_by_xpath('/html/body/app-root/div/div[1]/div/button').click()
-    sleep(1)
-    driver.find_element_by_xpath('/html/body/app-root/div/div[1]/div/div[2]/div/div/div[1]/table/tbody/tr[1]/td/button').click()
-    #교육청 - 경기도
-    driver.find_element_by_xpath('/html/body/app-root/div/div[1]/div/div[2]/div/div/div[3]/div/div/div/div[2]/div[1]/table/tbody/tr[1]/td/select').click()
-    driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[1]/table/tbody/tr[1]/td/select/option[10]').click()
-    #학교급 - 고등학교
-    driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[1]/table/tbody/tr[2]/td/select').click()
-    driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[1]/table/tbody/tr[2]/td/select/option[5]').click()
-    #학교검색
-    driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[1]/table/tbody/tr[3]/td[1]/input').send_keys("ㅇㅇ고등학교")
-    driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[1]/table/tbody/tr[3]/td[2]').click()
-    sleep(1)
-    driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[1]/ul').click()
-    driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[2]').click()
-    sleep(1)
-    driver.find_element_by_xpath('//*[@id="WriteInfoForm"]/table/tbody/tr[2]/td/input').send_keys(n)
-    driver.find_element_by_xpath('//*[@id="WriteInfoForm"]/table/tbody/tr[3]/td/input').send_keys(b)
-    sleep(2)
-    #PASSWORD
-    driver.find_element_by_xpath('/html/body/app-root/div/div[1]/div/div[2]/div/div/div[1]/table/tbody/tr/td/input').send_keys(p)
-    driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
-    sleep(1)
-    #SELECT_STUDENT
-    driver.find_element_by_xpath('//*[@id="container"]/div[2]/section[2]/div[2]/ul/li/a/button').click()
-    #SELFCK
-    sleep(1)
-    driver.find_element_by_css_selector('#container > div.subpage > div > div:nth-child(2) > div.survey_question > dl:nth-child(1) > dd > ul > li:nth-child(1) > label').click()   #37.5미만
-    driver.find_element_by_css_selector('#container > div.subpage > div > div:nth-child(2) > div.survey_question > dl:nth-child(2) > dd > ul > li:nth-child(1) > label').click()   #기저질환 아니오
-    driver.find_element_by_css_selector('#container > div.subpage > div > div:nth-child(2) > div.survey_question > dl:nth-child(3) > dd > ul > li:nth-child(1) > label').click()   #해외여행 아니오
-    driver.find_element_by_css_selector('#container > div.subpage > div > div:nth-child(2) > div.survey_question > dl:nth-child(4) > dd > ul > li:nth-child(1) > label').click()   #동거가족 해외여행 아니오
-    driver.find_element_by_css_selector('#container > div.subpage > div > div:nth-child(2) > div.survey_question > dl:nth-child(5) > dd > ul > li:nth-child(1) > label').click()   #동거가족 자가격리 아니오
-    sleep(1)
-    driver.find_element_by_css_selector('#btnConfirm').click()
-    sleep(1)
-    #RELOAD
-    driver.find_element_by_css_selector('#topMenuBtn').click()
-    driver.find_element_by_css_selector('#topMenuWrap > li:nth-child(2) > a').click()
-    #tackle ALERT
-    alert = driver.switch_to.alert
-    alert.accept()
-    sleep(1)
-    #TO_LOGIN_OTHER_ACCOUNT
-    driver.find_element_by_css_selector('body > app-root > div > div.secondary_pw > div > button').click()
-    alert.accept()
-    sleep(2)
 
+driver = webdriver.Chrome("chromedriver.exe")
+def jindan(n,b):
+    url = "https://eduro.goe.go.kr/stv_cvd_co00_002.do"
+    driver.get(url)
+    driver.find_element_by_name("frnoRidno").send_keys(b)  # 생년월일
+    driver.find_element_by_name("pName").send_keys(n)  # 이름
+    driver.find_element_by_name("schulNm").send_keys('돌마고등학교')  # 학교
+    driver.implicitly_wait(3)
+    # 확인버튼 xpath 값 가져오기
+    driver.find_element_by_xpath('/html/body/app-root/div[2]/section/div/div/div/div[3]/div/button').click()
+    driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
+
+    driver.find_element_by_xpath('//*[@id="rspns011"]').click()  # 발열
+    driver.find_element_by_xpath('//*[@id="rspns02"]').click()  # 아니오
+    driver.find_element_by_xpath('//*[@id="rspns070"]').click()  # 해외여행
+    driver.find_element_by_xpath('//*[@id="rspns080"]').click()  # 가족 유증상자
+    driver.find_element_by_xpath('//*[@id="rspns090"]').click()  # 가족 자가격리자
+    #sleep(1)   #차단 방지
+    ##제출 조심!
+    driver.find_element_by_xpath('/html/body/app-root/div[2]/section/div/div/div/form/div/div/button').click()   #제출
+    #sleep(1)   #차단방지
+    driver.find_element_by_xpath('//*[@id="container"]/div/div/div/div[2]/button').click()   #처음으로
+    #driver.close()
